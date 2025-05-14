2a8a461c3088be8635e2a734800d86c15fb68680
Pelinsu Kahraman <pelinsumarinette@gmail.com>
2025-05-14 22:46:39 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,26 +1,26 @@
 <?php
-$username = $_POST['username'] ?? '';
-$password = $_POST['password'] ?? '';
+$kullaniciadi = $_POST['kullaniciadi'] ?? '';
+$sifre = $_POST['sifre'] ?? '';
 
-if (empty($username) || empty($password)) {
+if (empty($kullaniciadi) || empty($sifre)) {
     echo "<script>alert('Kullanıcı adı ve şifre boş olamaz. Lütfen tekrar deneyin.'); window.location.href='giris.html';</script>";
     exit;
 }
 
-if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+if (!filter_var($kullaniciadi, FILTER_VALIDATE_EMAIL)) {
     echo "<script>alert('Geçersiz kullanıcı adı. Lütfen geçerli bir e-posta adresi girin.'); window.location.href='giris.html';</script>";
     exit;
 }
 
 $pattern = "/^b\d{9}@sakarya\.edu\.tr$/";
-if (!preg_match($pattern, $username)) {
+if (!preg_match($pattern, $kullaniciadi)) {
     echo "<script>alert('Kullanıcı adı geçerli bir öğrenci e-posta adresi olmalıdır.'); window.location.href='giris.html';</script>";
     exit;
 }
 
-$student_number = substr($username, 0, 10); 
-if ($password === $student_number) {
-    echo "<script>alert('Hoşgeldiniz, $student_number!'); window.location.href='index.html';</script>";
+$ogrenci_numarasi = substr($kullaniciadi, 0, 10); 
+if ($sifre === $ogrenci_numarasi) {
+    echo "<script>alert('Hoşgeldiniz, $ogrenci_numarasi!'); window.location.href='index.html';</script>";
 } else {
     echo "<script>alert('Kullanıcı adı veya şifre hatalı. Lütfen tekrar deneyin.'); window.location.href='giris.html';</script>";
 }
